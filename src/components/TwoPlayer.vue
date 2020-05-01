3f545bb5d995c47ca31ced5cc06341a7e0d404d9
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-05-01 14:37:15 -0400

    test

--- src/components/TwoPlayer.vue
+++ src/components/TwoPlayer.vue
@@ -340,7 +340,7 @@ export default {
         if (data.time) this.time = data.time;
         if (data.end) this.end = data.end;
         if (data.api_text) this.apiText = data.api_text;
-        console.log("p1text",this.p1Text,"p2Text",this.p2Text,"winner",this.winner, "end",this.end)
+        console.log("p1text",this.p1Text === true,"p2Text",this.p2Text === true,"winner",this.winner === true, "end",this.end === true)
         if (!this.player2 && this.userIs === "player1" && !this.end) {
           this.prompt = "Click Link To Copy";
         }
