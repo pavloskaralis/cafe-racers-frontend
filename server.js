a6856902e9dc1017c07b81b0ad6586949f512236
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-05-01 00:40:15 -0400

    test

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 8080;
-app.listen(port);
+// const express = require('express');
+// const serveStatic = require("serve-static")
+// const path = require('path');
+// app = express();
+// app.use(serveStatic(path.join(__dirname, 'dist')));
+// const port = process.env.PORT || 8080;
+// app.listen(port);
 
 // //Install express server
 // const express = require('express');
@@ -22,3 +22,20 @@ app.listen(port);
 
 // // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080);
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
